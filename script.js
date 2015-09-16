a7e879a173be0371ce8ed9878668b9f2ceccd336
Misty Morreyn <misty.morreyn@gmail.com>
2015-09-16 16:25:37 -0700

    Complete Assignment 1 Files

---
+++ script.js
@@ -0,0 +1,241 @@
+/* 
+    TITLE: ASSIGNMENT 1 JAVASCRIPT
+    AUTHOR: MISTY MORREYN
+    CLASS: COMM644 FALL 2015
+*/
+
+/* STEP 1:
+
+var someMonth;
+function theMonth;
+currentMonth;
+var summerMonth;
+myLibraryAFunction;
+
+*/
+
+/* STEP 2:
+
+3.9; // numeric literal expression
+"Hello World!"; // string literal expression
+true; // Boolean literal expression
+null; // null literal expression
+
+*/
+
+/* STEP 3:
+
+var myName = "not your ex wife Misty";
+var expression = "I am " + myName;
+
+*/
+
+/* STEP 4:
+
+var firstName, lastName, address, city, state, zipCode, yourAge, referralSource, strMayWeContactYou;
+
+*/
+
+/*  STEP 5:
+
+var firstName;
+var lastName;
+var address;
+firstName = "Misty";
+lastName = "Morreyn";
+address = "N/A";
+
+OR
+
+var city = "San Deigo";
+var state = "CA";
+var zipCode = 92108;
+
+OR
+
+var yourAge = 26, referralSource = "interwebs", strMayWeContactYou = "no";
+
+*/
+
+/* STEP 6:
+
+var numString = 5 + "10";
+console.log(numString);
+
+var booString = true + " or false?";
+console.log(booString);
+
+var numBoo = 5 + true;
+console.log(numBoo);
+
+*/
+
+/* STEP 7:
+
+var action = "make you";
+console.log("Does this class " + action + " " + question);
+var question = "cry?";
+
+ */
+
+/* STEP 8: 
+
+// no, it's not valid. Added backslash to make apostrophe recognized as part of string in console.
+
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."?';
+console.log(someString);
+
+*/
+
+/* STEP 9: 
+
+var x = null;
+console.log(x); //null value returned
+
+var y;
+console.log(y); //undefined value returned
+
+*/
+
+/* STEP 10: 
+
+var str = "Hello World!";
+console.log(typeof str);
+
+var num = 10;
+console.log(typeof num);
+
+var boo = true;
+console.log(typeof boo);
+
+var obj = {name: 'Morreyn', activity: 'Homework'};
+console.log(typeof obj);
+
+var x;
+console.log(typeof x);
+
+*/
+
+/* STEP 11:
+
+alert("Hello " + "Morreyn" + ", welcome to the Javascript class!");
+
+*/
+
+
+/* STEP 12: 
+
+var name = "Morreyn";
+alert("Hello " + name + ", welcome to the Javascript class!");
+
+*/
+
+/* STEP 13: 
+
+var name = "Morreyn";
+var course = "Javascript";
+alert("Hello " + name + ", welcome to the " + course + " class!");
+
+*/
+
+/* STEP 14:
+
+var name = "Morreyn";
+var course = "Javascript";
+alert("Hello " + name + ",\nWelcome to the " + course + " class!");
+
+*/
+
+/* STEP 15:
+
+var name = prompt("What is your name?");
+var course = "Javascript";
+alert("Hello " + name + ",\nWelcome to the " + course + " class!");
+
+*/
+
+/* STEP 16:
+
+var name = prompt("What is your name?");
+var course = prompt("What course are you taking?");
+alert("Hello " + name + ",\nWelcome to the " + course + " class!");
+
+*/
+
+/* STEP 17:
+
+var x = 10;
+var y = 20;
+console.log(x + y);
+
+*/
+
+/* STEP 18:
+
+var x = 20;
+x += 20;
+console.log(x);
+
+*/
+
+/* STEP 19:
+
+var x = 20;
+x *= 5;
+console.log(x);
+
+*/
+
+/* STEP 20:
+
+var x = 20 % 3;
+x /= 1;
+console.log(x);
+
+*/
+
+/* STEP 21:
+
+var x = 5;
+var y = 10;
+console.log(x != y && x < y);
+
+*/
+
+/* STEP 22: 
+
+var x = 5;
+var y = 10;
+console.log(x === y || x >= y);
+
+*/
+
+/* STEP 23: 
+
+var widget = new Object ();
+console.log(typeof widget);
+
+*/
+
+/* STEP 24: 
+
+var widget = new Object ();
+console.log(widget instanceof Object);
+
+*/
+    
+/* STEP 25:
+
+var widget = new Object ();
+console.log(widget instanceof Boolean);
+
+*/
+
+
+
+
+
+
+
+
+
